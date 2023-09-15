643b5a7ab7d16f0bd9433ff7a7242d12aac24180
FearlessSpiff <fearless.spiff@me.com>
2023-09-15 13:03:43 +0200

    introduce prod profile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8989'
+      '/api': 'http://localhost:8666'
     },
     host: '0.0.0.0'
   }
