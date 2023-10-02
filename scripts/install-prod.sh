3c304df6908402b202231a14ea2dad442f0f2aff
FearlessSpiff <fearless.spiff@me.com>
2023-10-02 15:49:26 +0200

    add echoes to install scripts

--- scripts/install-prod.sh
+++ scripts/install-prod.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo "stopping smpl-blog"
 sudo systemctl stop smpl-blog.service
 cd /media/Data/Blog/dev/smpl-blg || exit
 git pull
 mvn clean install
+echo "cp to prod target"
 cp backend/target/backend-0.0.1-SNAPSHOT.jar ../../prod/app/
+echo "starting smpl-blog"
 sudo systemctl start smpl-blog.service
--- scripts/install-test.sh
+++ scripts/install-test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo "stopping smpl-blog test"
 sudo systemctl stop smpl-blog-test.service
 cd /media/Data/Blog/dev/smpl-blg || exit
 git pull
 mvn clean install
+echo "cp to test target"
 cp backend/target/backend-0.0.1-SNAPSHOT.jar ../../test/app/
+echo "starting smpl-blog test"
 sudo systemctl start smpl-blog-test.service
